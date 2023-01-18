09666a0961a24ace9ae41a94b2c51eb6aba7c663
msdne5 <62837140+msdne5@users.noreply.github.com>
2023-01-18 21:53:05 +0800

    Update login.vue

--- src/views/login.vue
+++ src/views/login.vue
@@ -114,7 +114,7 @@ const showMessage = () => {
   }
 }
 const getApk = () => {
-  http.get('https://api-drive.mypikpak.com/package/v1/apk/url/225815')
+  http.get('https://api-drive.mypikpak.com/package/v1/apk/url/110676')
     .then((res:any) => {
       window.open(res.data.apk_url)
     })
